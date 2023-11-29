a86700fff0371fc8c0a54fca2a6df70af55dbef0
PaulKreft <64103063+PaulKreft@users.noreply.github.com>
2023-11-29 17:20:50 +0100

    format test class

--- src/test/java/MethodsChallengesTest.java
+++ src/test/java/MethodsChallengesTest.java
@@ -5,19 +5,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class MethodsChallengesTest {
 
     @Test
-    public void additionTest_when15and17_then32(){
+    public void additionTest_when15and17_then32() {
         // GIVEN
         int a = 15;
         int b = 17;
 
         // WHEN
-        int actual = MethodsChallenges.addition(a,b);
+        int actual = MethodsChallenges.addition(a, b);
 
         // THEN
         assertEquals(32, actual);
     }
+
     @Test
-    public void isEvenTest_when7_thenFalse(){
+    public void isEvenTest_when7_thenFalse() {
         // GIVEN
         int a = 7;
 
@@ -29,38 +30,40 @@ class MethodsChallengesTest {
     }
 
     @Test
-    public void multiplicationTest_when3and5_then15(){
+    public void multiplicationTest_when3and5_then15() {
         // GIVEN
         int a = 3;
         int b = 5;
 
         // WHEN
-        int actual = MethodsChallenges.multiplication(a,b);
+        int actual = MethodsChallenges.multiplication(a, b);
 
         // THEN
         assertEquals(15, actual);
-         }
-        @Test
-    public void convertToUpperCaseTest_whenHallo_Tamana_thenHALLO_TAMANA(){
-        // GIVEN
-            String a = "Hallo Tamana";
-            
-        // WHEN
-            String actual = MethodsChallenges.convertToUpperCase(a);
-            
-        // THEN
-            assertEquals("HALLO TAMANA", actual);
     }
-     @Test
+
+    @Test
+    public void convertToUpperCaseTest_whenHallo_Tamana_thenHALLO_TAMANA() {
+        // GIVEN
+        String a = "Hallo Tamana";
+
+        // WHEN
+        String actual = MethodsChallenges.convertToUpperCase(a);
+
+        // THEN
+        assertEquals("HALLO TAMANA", actual);
+    }
+
+    @Test
     public void checkIfPositiveTest_when0_thenFalse() {
-         // GIVEN
-         int a = 0;
+        // GIVEN
+        int a = 0;
 
-         // WHEN
-         boolean actual = MethodsChallenges.checkIfPositive(a);
+        // WHEN
+        boolean actual = MethodsChallenges.checkIfPositive(a);
 
-         // THEN
-         assertFalse(actual);
-     }
+        // THEN
+        assertFalse(actual);
+    }
 
 }
