c44d0b427106f8eef4973beb6ed961c85e5fc63f
PaulKreft <64103063+PaulKreft@users.noreply.github.com>
2023-11-29 16:26:38 +0100

    rename old tests; add new tests for remaining methods

--- src/main/java/MethodsChallenges.java
+++ src/main/java/MethodsChallenges.java
@@ -1,19 +1,23 @@
 public class MethodsChallenges {
 
-        public static int addition(int a, int b){
-            return a + b;
-        }
-        public static boolean isEven(int a){
-            return a%2 == 0;
-        }
-        public static int multiplication(int a, int b){
-            return a * b;
-        }
-        public static String shiftCase(String lowerCase){
-            return lowerCase.toUpperCase();
-        }
-        public static boolean checkIfPositive(int a){
-            return a>0;
-        }
+    public static int addition(int a, int b) {
+        return a + b;
     }
+
+    public static boolean isEven(int a) {
+        return a % 2 == 0;
+    }
+
+    public static int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public static String convertToUpperCase(String lowerCase) {
+        return lowerCase.toUpperCase();
+    }
+
+    public static boolean checkIfPositive(int a) {
+        return a > 0;
+    }
+}
 
--- src/test/java/MethodsChallengesTest.java
+++ src/test/java/MethodsChallengesTest.java
@@ -5,18 +5,62 @@ import static org.junit.jupiter.api.Assertions.*;
 class MethodsChallengesTest {
 
     @Test
-    public void testAddition_when15and17_then32(){
+    public void additionTest_when15and17_then32(){
+        // GIVEN
         int a = 15;
         int b = 17;
+
+        // WHEN
         int actual = MethodsChallenges.addition(a,b);
-        int expected = 32;
-        assertEquals(expected,actual);
+
+        // THEN
+        assertEquals(32, actual);
     }
     @Test
-    public void isEven_when7_isEven(){
-        boolean actual = MethodsChallenges.isEven(7);
+    public void isEvenTest_when7_thenFalse(){
+        // GIVEN
+        int a = 7;
 
+        // WHEN
+        boolean actual = MethodsChallenges.isEven(a);
+
+        // THEN
         assertFalse(actual);
     }
+
+    @Test
+    public void multiplicationTest_when3and5_then15(){
+        // GIVEN
+        int a = 3;
+        int b = 5;
+
+        // WHEN
+        int actual = MethodsChallenges.multiplication(a,b);
+
+        // THEN
+        assertEquals(15, actual);
+         }
+        @Test
+    public void convertToUpperCaseTest_whenHallo_Tamana_thenHALLO_TAMANA(){
+        // GIVEN
+            String a = "Hallo Tamana";
+            
+        // WHEN
+            String actual = MethodsChallenges.convertToUpperCase(a);
+            
+        // THEN
+            assertEquals("HALLO TAMANA", actual);
+    }
+     @Test
+    public void checkIfPositiveTest_when0_thenReturnFalse() {
+         // GIVEN
+         int a = 0;
+
+         // WHEN
+         boolean actual = MethodsChallenges.checkIfPositive(a);
+
+         // THEN
+         assertFalse(actual);
+     }
 
 }
