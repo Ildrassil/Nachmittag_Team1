9be45f218984c6db1249d09635fb4c12467ea98b
Ildrassil <schneiderjakob7@googlemail.com>
2023-11-29 15:50:09 +0100

    Add: Collaboration

--- src/main/java/MethodsChallenges.java
+++ src/main/java/MethodsChallenges.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class MethodsChallenges {
-}
+public class MethodsChallenges {
+
+        public static int addition(int a, int b){
+            return a + b;
+        }
+        public static boolean isEven(int a){
+            return a%2 == 0;
+        }
+        public static int multiplication(int a, int b){
+            return a * b;
+        }
+        public static String shiftCase(String lowerCase){
+            return lowerCase.toUpperCase();
+        }
+        public static boolean checkIfPositive(int a){
+            return a>0;
+        }
+    }
+
--- src/test/java/MethodsChallengesTest.java
+++ src/test/java/MethodsChallengesTest.java
@@ -1,4 +1,22 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MethodsChallengesTest {
-  
+
+    @Test
+    public void testAddition_when15and17_then32(){
+        int a = 15;
+        int b = 17;
+        int actual = MethodsChallenges.addition(a,b);
+        int expected = 32;
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void isEven_when7_isEven(){
+        boolean actual = MethodsChallenges.isEven(7);
+
+        assertFalse(actual);
+    }
+
 }
